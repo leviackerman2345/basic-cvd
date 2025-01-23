785798b8071526bd94e11cc6e5a3f63f233aa5b6
leviackerman2345 <cdegoma47@gmail.com>
2025-01-24 07:59:11 +0800

    fuck

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ burgir.addEventListener("click", () => {
   burgir.classList.toggle("open");
   headerNav.classList.toggle("open");
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector(".contact__form");
+  const popup = document.getElementById("popup");
+  const closePopup = document.getElementById("closePopup");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault(); 
+    popup.classList.remove("hidden");
+    form.reset();
+  });
+  closePopup.addEventListener("click", () => {
+    popup.classList.add("hidden");
+  });
+});
+
+
+const b2Top = document.querySelector(".backToTop");
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 200) {
+    b2Top.classList.add("open");
+  } else {
+    b2Top.classList.remove("open");
+  }
+})
